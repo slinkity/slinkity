788cb3febfea3c8cc20b38f7d2304302bf9b0760
Holben888 <bholmesdev@gmail.com>
2021-07-25 19:49:54 -0500

    show off React component imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import Counter from './components/Counter.jsx'
 
-const Home = () => <p>Home page</p>
+const Home = () => (
+  <main>
+    <h1>Home page</h1>
+    <Counter />
+  </main>
+)
 
 export default Home
