5d701a844d29a4f09bfe0deb1aac6bb1a9f15842
Benjamin Holmes <bholmesdev@gmail.com>
2021-08-12 21:53:48 -0500

    Fix Netlify deploy issues (#6)
    * add explicit install for latest 11ty canary
    
    * remove unsupported node syntax
    
    * remove eleventy-plugin-sass (slinkity processes these for us!)

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,9 +1,7 @@
-const pluginSass = require('eleventy-plugin-sass')
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const yaml = require('js-yaml')
 
 module.exports = function config(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginSass)
   eleventyConfig.addPassthroughCopy('assets')
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents))
   eleventyConfig.addPlugin(syntaxHighlight)
