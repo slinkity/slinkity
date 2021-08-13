c5aceb45f48f8bf654199cccea8cf18c81767ff0
Holben888 <bholmesdev@gmail.com>
2021-08-13 09:12:23 -0500

    fix: revert docs back to using eleventy (temporary)

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,10 +1,12 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const sassPlugin = require('eleventy-plugin-sass')
 const yaml = require('js-yaml')
 
 module.exports = function config(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents))
   eleventyConfig.addPlugin(syntaxHighlight)
+  eleventyConfig.addPlugin(sassPlugin)
 
   return {
     htmlTemplateEngine: 'njk',
