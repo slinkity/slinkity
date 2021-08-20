53d94ad1c3a4fc2eb0af488cc38d57eaf56e4909
bholmesdev <benjamin.holmes@onepeloton.com>
2021-08-20 16:01:24 -0500

    feat: escape html used as value in toHtmlAttrString

---
+++ src/utils/toEscapedHtml.js
@@ -0,0 +1,9 @@
+module.exports = function toEscapedHtml(value) {
+  // https://stackoverflow.com/a/7382028
+  return value
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+}
--- src/utils/toHtmlAttrString.js
+++ src/utils/toHtmlAttrString.js
@@ -1,7 +1,9 @@
+const toEscapedHtml = require('./toEscapedHtml')
+
 module.exports = function toHtmlAttrString(props = {}) {
   return Object.entries(props)
     .reduce((htmlAttrs, [key, value]) => {
-      return [...htmlAttrs, `${key}="${value}"`]
+      return [...htmlAttrs, `${key}="${toEscapedHtml(value)}"`]
     }, [])
     .join(' ')
 }
