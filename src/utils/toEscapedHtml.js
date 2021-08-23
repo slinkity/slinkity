061d7c23582a33f64cb961a6d7b35e8387486c71
Holben888 <bholmesdev@gmail.com>
2021-08-23 13:19:40 -0500

    refactor: add docs toHtmlAttrString

--- src/utils/toEscapedHtml.js
+++ src/utils/toEscapedHtml.js
@@ -1,6 +1,11 @@
+/**
+ * Escapes potentially invalid HTML characters for a given string
+ * @param {string} value String to escape
+ * @returns Escaped string for use in Html
+ */
 module.exports = function toEscapedHtml(value) {
   // https://stackoverflow.com/a/7382028
-  return `${value}`
+  return value
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
--- src/utils/toHtmlAttrString.js
+++ src/utils/toHtmlAttrString.js
@@ -1,9 +1,15 @@
 const toEscapedHtml = require('./toEscapedHtml')
 
+/**
+ * Converts an object of mixed types to a single string of Html attributes
+ * Ex. { key: 'value', number: 42 } -> `key="value" number="42"`
+ * @param {object} props Set of key / value pairs to stringify
+ * @returns String of Html attributes to apply to an element
+ */
 module.exports = function toHtmlAttrString(props = {}) {
   return Object.entries(props)
     .reduce((htmlAttrs, [key, value]) => {
-      return [...htmlAttrs, `${key}="${toEscapedHtml(value)}"`]
+      return [...htmlAttrs, `${key}="${toEscapedHtml(`${value}`)}"`]
     }, [])
     .join(' ')
 }
