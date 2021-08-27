886bfe576bfd9994e5e8282161633ae543b5264e
Holben888 <bholmesdev@gmail.com>
2021-08-27 16:47:43 -0500

    fix: move doc assets to /public to avoid build strip

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -2,7 +2,7 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const yaml = require('js-yaml')
 
 module.exports = function config(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('public')
   eleventyConfig.addPassthroughCopy('styles')
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents))
   eleventyConfig.addPlugin(syntaxHighlight)
