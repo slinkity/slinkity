d48213ea319da65b07a82abf83bfa557093a33c9
Holben888 <bholmesdev@gmail.com>
2021-08-27 16:33:51 -0500

    docs: upgrade to slinkity

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,12 +1,11 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
-const sassPlugin = require('eleventy-plugin-sass')
 const yaml = require('js-yaml')
 
 module.exports = function config(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets')
+  eleventyConfig.addPassthroughCopy('styles')
   eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents))
   eleventyConfig.addPlugin(syntaxHighlight)
-  eleventyConfig.addPlugin(sassPlugin)
 
   return {
     htmlTemplateEngine: 'njk',
