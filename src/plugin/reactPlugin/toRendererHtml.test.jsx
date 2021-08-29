0cfeb4f2d47322b8cd6807c370da79eda85d32cf
Holben888 <bholmesdev@gmail.com>
2021-08-28 21:41:20 -0500

    feat: add react to eslint rules

--- src/plugin/reactPlugin/toRendererHtml.test.jsx
+++ src/plugin/reactPlugin/toRendererHtml.test.jsx
@@ -1,6 +1,6 @@
 const toRendererHtml = require('./toRendererHtml')
 const React = require('react')
-const {parse} = require('node-html-parser')
+const { parse } = require('node-html-parser')
 const { SLINKITY_ATTRS, SLINKITY_REACT_MOUNT_POINT } = require('../../utils/consts')
 
 describe('toRendererHtml', () => {
@@ -15,8 +15,8 @@ describe('toRendererHtml', () => {
           </ul>
         </>
       )
-  
-      expect(toRendererHtml({Component, render: 'static'})).toMatchSnapshot()
+
+      expect(toRendererHtml({ Component, render: 'static' })).toMatchSnapshot()
     })
   })
   describe('render: eager', () => {
@@ -25,18 +25,24 @@ describe('toRendererHtml', () => {
         <section>
           <h2>Our eager hydration</h2>
           <p>Is extremely useful</p>
-          <p>Learn more about it <a href="https://slinkity.dev/docs/partial-hydration/">here</a></p>
+          <p>
+            Learn more about it <a href="https://slinkity.dev/docs/partial-hydration/">here</a>
+          </p>
         </section>
       )
       const componentPath = '/very/cool.jsx'
-  
-      expect(toRendererHtml({Component, render: 'eager', componentPath })).toMatchSnapshot()
+
+      expect(toRendererHtml({ Component, render: 'eager', componentPath })).toMatchSnapshot()
     })
     it('should apply props as additional data attributes', () => {
       const Component = ({ logo, links }) => (
         <nav>
           <img src={logo.src} alt={logo.alt} />
-          {links.map(({href, label}) => <a key={href} href={href}>{label}</a>)}
+          {links.map(({ href, label }) => (
+            <a key={href} href={href}>
+              {label}
+            </a>
+          ))}
         </nav>
       )
       const componentPath = '_includes/components/nice-nav.jsx'
@@ -45,26 +51,30 @@ describe('toRendererHtml', () => {
           src: 'accessible-logo.png',
           alt: 'Great logo',
         },
-        links: [{
-          href: '/home',
-          label: 'Home',
-        }, {
-          href: '/about',
-          label: 'About',
-        }, {
-          href: '/contact',
-          label: 'Contact',
-        }]
+        links: [
+          {
+            href: '/home',
+            label: 'Home',
+          },
+          {
+            href: '/about',
+            label: 'About',
+          },
+          {
+            href: '/contact',
+            label: 'Contact',
+          },
+        ],
       }
-  
-      expect(toRendererHtml({Component, render: 'eager', componentPath, props })).toMatchSnapshot()
+
+      expect(toRendererHtml({ Component, render: 'eager', componentPath, props })).toMatchSnapshot()
     })
   })
   describe('render: lazy', () => {
     it('should add lazy as a data attribute', () => {
       const Component = () => <p>Not important</p>
 
-      const output = toRendererHtml({Component, render: 'lazy' })
+      const output = toRendererHtml({ Component, render: 'lazy' })
       const mountPoint = parse(output).querySelector(SLINKITY_REACT_MOUNT_POINT)
 
       expect(mountPoint.getAttribute(SLINKITY_ATTRS.lazy)).toEqual('true')
