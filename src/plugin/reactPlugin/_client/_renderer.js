6818dfdfbd163614cb7ec0ba8ed3e54f10ca158b
Holben888 <bholmesdev@gmail.com>
2021-08-29 16:59:57 -0500

    rebase onto main renderer

--- src/plugin/reactPlugin/_client/_renderer.js
+++ src/plugin/reactPlugin/_client/_renderer.js
@@ -11,20 +11,6 @@ export default function renderComponent({
   const mountPoint = document.querySelector(
     `${SLINKITY_REACT_MOUNT_POINT}[${SLINKITY_ATTRS.path}="${componentPath}"][${SLINKITY_ATTRS.instance}="${instance}"]`,
   )
-  const innerReactEl = mountPoint.querySelector(
-    `${SLINKITY_REACT_MOUNT_POINT}[${SLINKITY_ATTRS.type}="page"]`,
-  )
 
-  let children
-  if (innerReactEl) {
-    const childrenProps = {
-      dangerouslySetInnerHTML: { __html: innerReactEl.innerHTML },
-    }
-    for (const attribute of innerReactEl.attributes) {
-      childrenProps[attribute.name] = attribute.value
-    }
-    children = React.createElement(innerReactEl.tagName, childrenProps)
-  }
-
-  ReactDOM.render(React.createElement(Component, props, children), mountPoint)
+  ReactDOM.render(React.createElement(Component, props, null), mountPoint)
 }
