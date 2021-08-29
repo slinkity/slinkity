734bd985a2dd4c0aef9ac22544d1857ec49e2442
Holben888 <bholmesdev@gmail.com>
2021-08-29 14:53:55 -0500

    feat: add tests for fs writes

--- src/plugin/reactPlugin/toHydrationLoadersApplied.test.js
+++ src/plugin/reactPlugin/toHydrationLoadersApplied.test.js
@@ -1,12 +1,22 @@
 const { parse } = require('node-html-parser')
+const fsPromises = require('fs').promises
+const { join } = require('path')
 const { SLINKITY_REACT_MOUNT_POINT, SLINKITY_ATTRS } = require('../../utils/consts')
 const toHtmlAttrString = require('../../utils/toHtmlAttrString')
+const fileHelpers = require('../../utils/fileHelpers')
 const applyHtmlWrapper = require('./applyHtmlWrapper')
 const { toHydrationLoadersApplied, webComponentLoader } = require('./toHydrationLoadersApplied')
-const fsPromises = require('fs').promises
 
-jest.mock('fs/promises')
-jest.mock('../../utils/fileHelpers')
+const READ_FILE_CALLED = 'readFile called'
+
+jest.mock('fs', () => ({
+  promises: {
+    readFile: jest.fn().mockReturnValue(READ_FILE_CALLED),
+  },
+}))
+jest.mock('../../utils/fileHelpers', () => ({
+  writeFileRec: jest.fn().mockResolvedValue(),
+}))
 
 describe('toHydrationLoadersApplied', () => {
   describe('with no mount points', () => {
@@ -190,6 +200,91 @@ describe('toHydrationLoadersApplied', () => {
         isDryRun: true,
       })
       expect(actual).toMatchSnapshot()
+    })
+  })
+  describe('with dry run disabled', () => {
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+    it('should read the JSX file from the correct path', async () => {
+      const componentToPropsMap = {}
+      const toProps = (componentPath) => ({
+        [SLINKITY_ATTRS.path]: componentPath,
+      })
+      const dir = {
+        input: 'src',
+        output: '_site',
+      }
+      const toRelativeInput = (componentPath) => join(dir.input, componentPath)
+      const content = `<html>
+<head>
+  <title>It's hydration time</title>
+</head>
+<body>
+  <${SLINKITY_REACT_MOUNT_POINT} ${toHtmlAttrString(toProps('/nav.jsx'))}>
+  <nav>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </nav>
+  </${SLINKITY_REACT_MOUNT_POINT}>
+  <${SLINKITY_REACT_MOUNT_POINT} ${toHtmlAttrString(toProps('/nested/Heading.jsx'))}>
+    <h2>Welcome to the site</h2>
+  </${SLINKITY_REACT_MOUNT_POINT}>
+</body>
+</html>`
+
+      await toHydrationLoadersApplied({
+        content,
+        componentToPropsMap,
+        dir,
+        isDryRun: false,
+      })
+      expect(fsPromises.readFile).toHaveBeenCalledTimes(2)
+      expect(fsPromises.readFile.mock.calls).toEqual([
+        [toRelativeInput('/nav.jsx')],
+        [toRelativeInput('/nested/Heading.jsx')],
+      ])
+    })
+    it('should write the JSX file from the correct path', async () => {
+      const componentToPropsMap = {}
+      const toProps = (componentPath) => ({
+        [SLINKITY_ATTRS.path]: componentPath,
+      })
+      const dir = {
+        input: 'src',
+        output: '_site',
+      }
+      const toRelativeOutput = (componentPath) => join(dir.output, componentPath)
+      const content = `<html>
+<head>
+  <title>It's hydration time</title>
+</head>
+<body>
+  <${SLINKITY_REACT_MOUNT_POINT} ${toHtmlAttrString(toProps('/nav.jsx'))}>
+  <nav>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </nav>
+  </${SLINKITY_REACT_MOUNT_POINT}>
+  <${SLINKITY_REACT_MOUNT_POINT} ${toHtmlAttrString(toProps('/nested/Heading.jsx'))}>
+    <h2>Welcome to the site</h2>
+  </${SLINKITY_REACT_MOUNT_POINT}>
+</body>
+</html>`
+
+      await toHydrationLoadersApplied({
+        content,
+        componentToPropsMap,
+        dir,
+        isDryRun: false,
+      })
+      expect(fileHelpers.writeFileRec).toHaveBeenCalledTimes(2)
+      expect(fileHelpers.writeFileRec.mock.calls).toEqual([
+        [toRelativeOutput('/nav.jsx'), READ_FILE_CALLED],
+        [toRelativeOutput('/nested/Heading.jsx'), READ_FILE_CALLED],
+      ])
     })
   })
 })
