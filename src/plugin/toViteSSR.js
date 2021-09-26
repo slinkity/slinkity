52f7a718b405992f05d31ebcf32a8151e714b31f
Holben888 <bholmesdev@gmail.com>
2021-09-26 16:54:30 -0400

    feat: add Vite SSR helper

---
+++ src/plugin/toViteSSR.js
@@ -0,0 +1,84 @@
+const { createServer, build } = require('vite')
+const requireFromString = require('require-from-string')
+const logger = require('../utils/logger')
+
+/**
+ * @typedef ViteSSRParams
+ * @property {'dev' | 'prod'} environment - whether we want a dev server or a production build
+ * @property {import('./index').PluginOptions['dir']} dir
+ * @param {ViteSSRParams}
+ *
+ * @typedef {{
+ *  default: () => any;
+ *  getProps: (eleventyData: any) => any;
+ *  frontMatter: Record<string, any>;
+ *  __stylesGenerated: Record<string, string>;
+ * }} FormattedModule - expected keys from a given component module
+ *
+ * @typedef ViteSSR - available fns for module conversion
+ * @property {(filePath: string) => Promise<FormattedModule>} toCommonJSComponentModule - fn to grab a Node-friendly module output from a given file path
+ *
+ * @returns {ViteSSR} viteSSR
+ */
+module.exports = async function toViteSSR({ environment, dir }) {
+  if (environment === 'dev') {
+    const server = await createServer({
+      root: dir.output,
+      server: {
+        middlewareMode: 'ssr',
+      },
+      clearScreen: false,
+    })
+    return {
+      async toCommonJSComponentModule(filePath) {
+        return {
+          default: () => null,
+          getProps: () => ({}),
+          frontMatter: {},
+          __stylesGenerated: {},
+          ...(await server.ssrLoadModule(filePath)),
+        }
+      },
+    }
+  } else {
+    /**
+     * @type {Record<string, any>}
+     */
+    const probablyInefficientCache = {}
+    return {
+      async toCommonJSComponentModule(filePath) {
+        if (probablyInefficientCache[filePath]) return probablyInefficientCache[filePath]
+
+        const { output } = await build({
+          root: '_site',
+          build: {
+            ssr: true,
+            write: false,
+            rollupOptions: {
+              input: filePath,
+            },
+          },
+        })
+        if (!output?.length) {
+          logger.log({
+            type: 'error',
+            message: `Module ${filePath} didn't have any output. Is this file blank?`,
+          })
+          return {}
+        }
+        /**
+         * @type {FormattedModule}
+         */
+        const mod = {
+          default: () => null,
+          getProps: () => ({}),
+          frontMatter: {},
+          __stylesGenerated: {},
+          ...requireFromString(output[0].code),
+        }
+        probablyInefficientCache[filePath] = mod
+        return mod
+      },
+    }
+  }
+}
