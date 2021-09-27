94bf6de7108f05f882101726fe7993543b452cbe
Holben888 <bholmesdev@gmail.com>
2021-09-27 19:17:31 -0400

    feat: read component attrs by page inputPath

--- src/plugin/reactPlugin/toHydrationLoadersApplied.js
+++ src/plugin/reactPlugin/toHydrationLoadersApplied.js
@@ -2,7 +2,7 @@ const { parse } = require('node-html-parser')
 const { join } = require('path')
 const { readFile } = require('fs').promises
 const applyHtmlWrapper = require('./applyHtmlWrapper')
-const { SLINKITY_ATTRS, SLINKITY_REACT_MOUNT_POINT } = require('../../utils/consts')
+const { SLINKITY_REACT_MOUNT_POINT } = require('../../utils/consts')
 const { writeFileRec } = require('../../utils/fileHelpers')
 const toLoaderScript = require('./toLoaderScript')
 const toClientImportStatement = require('./toClientImportStatement')
@@ -28,96 +28,94 @@ function toStyleTag(styleToFilePathMap) {
 }
 
 const errorMessage = ({
-  id,
   inputPath,
-}) => `We failed to hydrate a mount point with the id ${id} in the file ${inputPath}
+  stacktrace,
+}) => `We failed to render components used in file ${inputPath}
 We recommend trying to:
 1. delete your output directory and restart the server / build
 2. clear your node_modules and running a clean install with "npm i"
 
-Visit https://slinkity.dev to review our changelogs!`
+Visit https://slinkity.dev to review our changelogs!
+Stacktrace:
+${stacktrace}`
 
 /**
  * Applies all necessary scripts to an HTML document for clientside hydration
  * 1. Looks for any mount points applied by component pages and shortcodes
  * 2. Applies the appropriate loader `<script>` for each (eager or lazy)
  * 3. Appends the mount point web component declaration as a `<script>`
+ * 4. Applies all styles imported by components as ES modules using inline `<style>` tags
  *
- * @param {{
- *   content: string,
- *   componentAttrStore: import('./componentAttrStore').ComponentAttrStore,
- *   dir?: {
- *     input: string,
- *     output: string,
- *   },
- *   isDryRun?: boolean,
- * }} params
- * @returns {string} output HTML content with all hydration scripts applied
+ * @typedef HydrationLoadersAppliedParams
+ * @property {string} content - markup template content passed by 11ty's HTML transform
+ * @property {import('./componentAttrStore').ComponentAttrsWithId[]} componentAttrs - all component attributes used for rendering and hydration
+ * @property {import('../index').PluginOptions['dir']} dir - input and output directories
+ * @property {boolean?} isDryRun - whether to write components to output directory. When isDryRun = true, no file system actions will occur
+ *
+ * @param {HydrationLoadersAppliedParams}
+ * @returns {string} output markup with every component's inline styles and hydration scripts applied
  */
-async function toHydrationLoadersApplied({ content, componentAttrStore, dir, isDryRun = false }) {
+async function toHydrationLoadersApplied({ content, componentAttrs, dir, isDryRun = false }) {
   const root = parse(content)
   applyHtmlWrapper(root)
-  const mountPoints = [...root.querySelectorAll(SLINKITY_REACT_MOUNT_POINT)]
 
-  if (mountPoints.length > 0) {
-    try {
-      // 1. Get the attributes for all mount points on the page
-      const hydrationAttrs = mountPoints.map((mountPoint) => {
-        const id = mountPoint.getAttribute(SLINKITY_ATTRS.id) || ''
-        const componentAttrs = componentAttrStore.get(parseInt(id))
-        if (!componentAttrs) {
-          throw new Error(errorMessage({ id, inputPath: this.inputPath }))
-        }
-        return { id, ...componentAttrs }
-      })
+  try {
+    // 1. Get the attributes for all components that need hydration
+    const componentAttrsWithHydration = componentAttrs.filter(({ hydrate }) => hydrate !== 'static')
 
-      // 2. Copy the associated component file to the output dir
-      if (!isDryRun && dir) {
-        await Promise.all(
-          hydrationAttrs.map(async ({ path: componentPath }) => {
-            const jsxInputPath = join(dir.input, componentPath)
-            const jsxOutputPath = join(dir.output, componentPath)
-            await writeFileRec(jsxOutputPath, await readFile(jsxInputPath))
-          }),
-        )
-      }
-
-      // 3. Generate scripts to hydrate our mount points
-      const componentScripts = hydrationAttrs.map(({ path: componentPath, hydrate, id, props }) =>
-        toLoaderScript({
-          componentPath,
-          hydrate,
-          id,
-          props,
+    // 2. Copy the associated component file to the output dir
+    if (!isDryRun && dir) {
+      await Promise.all(
+        componentAttrsWithHydration.map(async ({ path: componentPath }) => {
+          const jsxInputPath = join(dir.input, componentPath)
+          const jsxOutputPath = join(dir.output, componentPath)
+          await writeFileRec(jsxOutputPath, await readFile(jsxInputPath))
         }),
       )
-
-      // 4. Generate module for all component styles imported as ES modules (ex. CSS modules)
-      // Here, we flatten each component's generated styles into a single map for the page
-      const stylesStringified = toStyleTag(
-        hydrationAttrs.reduce((styleMap, { styleToFilePathMap }) => {
-          for (const [key, content] of Object.entries(styleToFilePathMap)) {
-            styleMap[key] = content
-          }
-          return styleMap
-        }, {}),
-      )
-
-      root
-        .querySelector('body')
-        .insertAdjacentHTML(
-          'beforeend',
-          `${webComponentLoader}${componentScripts.join('\n')}${stylesStringified}`,
-        )
-    } catch (e) {
-      // we silently fail so our error logs aren't buried by 11ty's
-      // TODO: handle Slinkity-specific exceptions at the CLI level
-      // so we can stop living in fear of exceptions
-      if (e?.message) {
-        log({ type: 'error', message: e.message })
-      }
-      return root.outerHTML
     }
+
+    // 3. Generate scripts to hydrate those components
+    const scripts = componentAttrsWithHydration.map(({ path: componentPath, hydrate, id, props }) =>
+      toLoaderScript({
+        componentPath,
+        hydrate,
+        id,
+        props,
+      }),
+    )
+
+    // 4. Generate a `<style>` for all component CSS imported as ES modules (ex. CSS modules)
+    // Here, we flatten each component's generated styles into a single map for the page
+    const stylesStringified = toStyleTag(
+      componentAttrs.reduce((styleMap, { styleToFilePathMap }) => {
+        for (const [key, content] of Object.entries(styleToFilePathMap)) {
+          styleMap[key] = content
+        }
+        return styleMap
+      }, {}),
+    )
+
+    root
+      .querySelector('body')
+      .insertAdjacentHTML(
+        'beforeend',
+        [
+          ...(componentAttrsWithHydration.length ? [webComponentLoader] : []),
+          ...scripts,
+          stylesStringified,
+        ].join('\n'),
+      )
+  } catch (e) {
+    // we silently fail so our error logs aren't buried by 11ty's
+    // TODO: handle Slinkity-specific exceptions at the CLI level
+    // so we can stop living in fear of exceptions
+    if (e?.message) {
+      log({
+        type: 'error',
+        message: errorMessage({ inputPath: dir.input, stacktrace: e.message }),
+      })
+    }
+    return root.outerHTML
   }
   return root.outerHTML
 }
