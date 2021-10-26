d61c346c755ce7e8eff947ea6ba03435b2524e36
Holben888 <bholmesdev@gmail.com>
2021-10-26 10:22:14 -0400

    refactor: change "defaults" to "ignores"

--- src/main/defineConfig.js
+++ src/main/defineConfig.js
@@ -1,13 +1,13 @@
 /**
  * @typedef UserSlinkityConfig
- * @property {string[] | (defaults: string[]) => string[]} eleventyIgnores All files (or globs) Slinkity will ask 11ty to ignore during builds and live reload events. Override this property to add and remove ignored files from our defaults. Also see 11ty's ignore documentation here: https://www.11ty.dev/docs/ignores/
+ * @property {string[] | (ignores: string[]) => string[]} eleventyIgnores All files (or globs) Slinkity will ask 11ty to ignore during builds and live reload events. Override this property to add and remove ignored files from our defaults. Also see 11ty's ignore documentation here: https://www.11ty.dev/docs/ignores/
  *
  * @param {Partial<UserSlinkityConfig>} userConfig
  * @returns {UserSlinkityConfig} formatted Slinkity Config with defaults applied for omitted keys
  */
 function defineConfig(userConfig = {}) {
   return {
-    eleventyIgnores: userConfig.eleventyIgnores ?? ((defaults) => defaults),
+    eleventyIgnores: userConfig.eleventyIgnores ?? ((ignores) => ignores),
   }
 }
 
