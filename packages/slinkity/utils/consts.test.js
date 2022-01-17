33fac26b0e51e71f5589b93b458c20f5dac94a0d
Paul Shryock (he/him) <paul@pshry.com>
2022-01-17 08:33:30 -0500

    [#21] Add unit test expectations

--- packages/slinkity/utils/consts.test.js
+++ packages/slinkity/utils/consts.test.js
@@ -1,9 +1,13 @@
 const { toSSRComment } = require('./consts')
 
 describe('toSSRComment', () => {
-  it('should throw an error if missing an ID', () => {
+  it('should throw without a valid ID', () => {
     const message = 'must pass non-empty string to toSSRComment'
     expect(() => toSSRComment()).toThrowError(new TypeError(message))
+    expect(() => toSSRComment('')).toThrowError(new TypeError(message))
+    expect(() => toSSRComment(123)).toThrowError(new TypeError(message))
+    expect(() => toSSRComment(['some-id'])).toThrowError(new TypeError(message))
+    expect(() => toSSRComment({})).toThrowError(new TypeError(message))
   })
 
   it('should return an SSR comment with build hash and ID', () => {
