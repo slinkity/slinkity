f1786f5c1712b39902fe10164716a99bbe54c500
Holben888 <bholmesdev@gmail.com>
2022-01-17 12:19:02 -0500

    refactor: move UserSlinkityConfig to d.ts

--- packages/slinkity/defineConfig.js
+++ packages/slinkity/defineConfig.js
@@ -1,6 +1,5 @@
 /**
- * @typedef UserSlinkityConfig
- * @property {string[] | (ignores: string[]) => string[]} eleventyIgnores All files (or globs) Slinkity will ask 11ty to ignore during builds and live reload events. Override this property to add and remove ignored files from our defaults. Also see 11ty's ignore documentation here: https://www.11ty.dev/docs/ignores/
+ * @typedef {import('./index').UserSlinkityConfig} UserSlinkityConfig
  *
  * @param {Partial<UserSlinkityConfig>} userConfig
  * @returns {UserSlinkityConfig} formatted Slinkity Config with defaults applied for omitted keys
--- packages/slinkity/index.d.ts
+++ packages/slinkity/index.d.ts
@@ -1,2 +1,8 @@
-export type { UserSlinkityConfig } from './lib/config/defineConfig'
 export { defineConfig } from './lib/config/defineConfig'
+
+export type UserSlinkityConfig = {
+  /**
+   * All files (or globs) Slinkity will ask 11ty to ignore during builds and live reload events. Override this property to add and remove ignored files from our defaults. Also see 11ty's ignore documentation here: https://www.11ty.dev/docs/ignores/
+   */
+  eleventyIgnores: string[] | ((ignores: string[]) => string[]);
+};
