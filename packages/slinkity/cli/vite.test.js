dce352e944c3f87c95b5cdff069130bb39c9830d
Paul Shryock (he/him) <paul@pshry.com>
2022-01-17 16:23:25 -0500

    Update unit tests and add JSDoc comment

--- packages/slinkity/cli/vite.test.js
+++ packages/slinkity/cli/vite.test.js
@@ -2,34 +2,38 @@ const { getConfigFile, getResolvedAliases } = require('./vite')
 const { resolve } = require('path')
 
 describe('vite', () => {
-  it('getConfigFile', async () => {
-    expect(await getConfigFile()).toBeUndefined()
+  describe('getConfigFile', () => {
+    it('returns undefined when vite config file does not exist', async () => {
+      expect(await getConfigFile()).toBeUndefined()
+    })
   })
 
-  it('getResolvedAliases', () => {
-    const empty = {
-      input: '',
-      includes: '',
-      layouts: '',
-    }
-    const emptyExpected = {
-      root: resolve(''),
-      input: resolve(''),
-      includes: resolve(''),
-      layouts: resolve(''),
-    }
-    const config = {
-      input: 'input',
-      includes: 'includes',
-      layouts: 'layouts',
-    }
-    const configExpected = {
-      root: resolve(''),
-      input: resolve(config.input),
-      includes: resolve(config.input, config.includes),
-      layouts: resolve(config.input, config.layouts),
-    }
-    expect(getResolvedAliases(empty)).toStrictEqual(emptyExpected)
-    expect(getResolvedAliases(config)).toStrictEqual(configExpected)
+  describe('getResolvedAliases', () => {
+    it('returns object with resolved aliases', () => {
+      const empty = {
+        input: '',
+        includes: '',
+        layouts: '',
+      }
+      const emptyExpected = {
+        root: resolve(''),
+        input: resolve(''),
+        includes: resolve(''),
+        layouts: resolve(''),
+      }
+      const config = {
+        input: 'input',
+        includes: 'includes',
+        layouts: 'layouts',
+      }
+      const configExpected = {
+        root: resolve(''),
+        input: resolve(config.input),
+        includes: resolve(config.input, config.includes),
+        layouts: resolve(config.input, config.layouts),
+      }
+      expect(getResolvedAliases(empty)).toStrictEqual(emptyExpected)
+      expect(getResolvedAliases(config)).toStrictEqual(configExpected)
+    })
   })
 })
