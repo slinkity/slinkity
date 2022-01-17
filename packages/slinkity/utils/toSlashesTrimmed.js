33313f037c046c2ec5c72c852faa9b9e88200f56
Paul Shryock (he/him) <paul@pshry.com>
2022-01-17 18:58:36 -0500

    Add toSlashesTrimmed unit test (#141)
    * Add toSlashesTrimmed unit tests
    
    * Add JSDoc comment

--- packages/slinkity/utils/toSlashesTrimmed.js
+++ packages/slinkity/utils/toSlashesTrimmed.js
@@ -1,3 +1,11 @@
-module.exports = function toSlashesTrimmed(url) {
+/**
+ * Returns a URL with leading and trailing slashes trimmed.
+ *
+ * @param  {string} url URL with or without slashes.
+ * @return {string}     URL with slashes trimmed.
+ */
+function toSlashesTrimmed(url) {
   return url.replace(/^\/|\/$/g, '')
 }
+
+module.exports = toSlashesTrimmed
