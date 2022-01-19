d5f675dd491cc75f5f68ffc959264f3c5ba0658e
Benjamin Holmes <bholmesdev@gmail.com>
2022-01-18 19:01:22 -0500

    BIG refactor: extract all React code to renderer (#143)
    * refactor: merge applyViteHtmlTrans params
    
    * chore: remove unused util
    
    * feat: add renderer-react draft
    
    * refactor: expose toCommonJSModule
    
    * feat: stub out addComponent helpers
    
    * fix: bad util imports
    
    * feat: get server and client working!
    
    * refactor: move UserSlinkityConfig to d.ts
    
    * refactor: move shared types to d.ts
    
    * feat: add helper for eleventy ignored
    
    * feat: use eleventy ignored helper
    
    * chore: remove console.log
    
    * feat: add Renderer type
    
    * chore: remove toRendererHtml logic
    
    * feat: remove react from jest preset
    
    * fix: keep optimizeDeps from astro
    
    * feat: remove tsc builder! (all .d.ts are written manually now)
    
    * refactor: add types.d.ts to each dir
    
    * feat: pull in vite configs from userSlinkityConfig
    
    * feat: change "render" to "hydrate"
    
    * fix: update package files after types restructure
    
    * tests: update applyViteHtmlTransform for renderers
    
    * refactor: rename to "injectImportedStyles"
    
    * docs: add JSdocs to Renderer types
    
    * docs: add Renderer type to component helpers
    
    * lint: remove unused vars
    
    * docs: note StaticHtml is lifted from Astro
    
    * chore: remove old reactPlugin dir
    
    * fix: move shortcodes test
    
    * fix: bad type references
    
    * fix: applyViteHtmlTransform types

---
+++ packages/slinkity/eleventyConfig/addComponentPages.js
@@ -0,0 +1,12 @@
+/**
+ * @typedef AddComponentPagesParams
+ * @property {import('./componentAttrStore').ComponentAttrStore} componentAttrStore
+ * @property {import('../cli/vite').ResolvedImportAliases} resolvedImportAliases
+ * @property {import('../cli/toViteSSR').ViteSSR} viteSSR
+ * @property {any} eleventyConfig
+ * @property {import('../cli/types').Renderer} renderer
+ * @param {AddComponentPagesParams}
+ */
+module.exports = function addComponentPages() {
+  // TODO
+}
