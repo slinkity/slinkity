16b43ea616377ef9762ff6267694ba08699ebaf0
Holben888 <bholmesdev@gmail.com>
2022-01-23 13:08:44 -0500

    feat: add initial Svelte renderer

---
+++ packages/slinkity-renderer-svelte/Wrapper.svelte
@@ -0,0 +1,27 @@
+<script>
+  /**
+   * Source: https://github.com/withastro/astro/blob/main/packages/renderers/renderer-svelte/Wrapper.svelte
+   *
+   * Why do we need a wrapper component?
+   *
+   * Slinkity passes `children` as a string of HTML, so we need
+   * a way to render that content.
+   *
+   * Rather than passing a magical prop which needs special
+   * handling, using this wrapper allows Svelte users to just
+   * use `<slot />` like they would for any other component.
+   */
+  const {
+    __slinkity_component: Component,
+    __slinkity_children,
+    ...props
+  } = $$props;
+</script>
+
+<svelte:component this={Component} {...props}>
+  {#if __slinkity_children != null}
+    <slinkity-fragment>
+      {@html __slinkity_children}
+    </slinkity-fragment>
+  {/if}
+</svelte:component>
