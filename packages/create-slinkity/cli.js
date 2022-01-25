9f7b9342659c95d7832092598831b1db9b9cc4c1
Benjamin Holmes <bholmesdev@gmail.com>
2022-01-24 21:07:15 -0500

    Feat: `create-slinkity` remaining updates for multi-framework support (#157)
    * refactor: change src/destRoot to src/destResolved
    
    * feat: generate the slinkity.config
    
    * feat: construct packagejson deps
    
    * feat: copy component exclusive templates
    
    * docs: update messaging to show components are optional
    
    * feat: generate nav links in layout

--- packages/create-slinkity/cli.js
+++ packages/create-slinkity/cli.js
@@ -4,7 +4,90 @@ const fs = require('fs')
 const { yellow, red } = require('kolorist')
 const prompts = require('prompts')
 
+const INDEX_MD = 'src/index.md'
+const LAYOUT_NJK = 'src/_includes/layout.njk'
 const PKG = 'package.json'
+const filesToPreprocess = new Set([INDEX_MD, PKG, LAYOUT_NJK])
+
+const componentFlavorMeta = {
+  react: {
+    packageJson: {
+      dependencies: {
+        react: '^17.0.2',
+        'react-dom': '^17.0.2',
+      },
+      devDependencies: {
+        '@slinkity/renderer-react': '^0.1.0',
+      },
+    },
+    slinkityConfig: {
+      renderer: 'reactRenderer',
+      importStatement: "import reactRenderer from '@slinkity/renderer-react'",
+    },
+    navLink: {
+      href: '/react-page',
+      text: 'React',
+    },
+    shortcode: '{% react "Slinky", hydrate="eager" %}',
+    exclusiveTemplates: ['src/react-page.jsx', 'src/_includes/Slinky.jsx', 'styles/slinky.scss'],
+  },
+  vue: {
+    packageJson: {
+      dependencies: {
+        vue: '^3.2.28',
+      },
+      devDependencies: {
+        '@slinkity/renderer-vue': '^0.1.0',
+      },
+    },
+    slinkityConfig: {
+      renderer: 'vueRenderer',
+      importStatement: "import vueRenderer from '@slinkity/renderer-vue'",
+    },
+    navLink: {
+      href: '/vue-page',
+      text: 'Vue',
+    },
+    shortcode: '{% vue "Slinky.vue", hydrate="eager" %}',
+    exclusiveTemplates: ['src/vue-page.vue', 'src/_includes/Slinky.vue'],
+  },
+  svelte: {
+    packageJson: {
+      dependencies: {
+        svelte: '^3.46.2',
+      },
+      devDependencies: {
+        '@slinkity/renderer-svelte': '^0.1.0',
+      },
+    },
+    slinkityConfig: {
+      renderer: 'svelteRenderer',
+      importStatement: "import svelteRenderer from '@slinkity/renderer-svelte'",
+    },
+    navLink: {
+      href: '/svelte-page',
+      text: 'Svelte',
+    },
+    shortcode: '{% svelte "Slinky.svelte", hydrate="eager" %}',
+    exclusiveTemplates: ['src/svelte-page.svelte', 'src/_includes/Slinky.svelte'],
+  },
+}
+
+function toSlinkityConfigContents(selectedComponentFlavors) {
+  const renderers = selectedComponentFlavors.map(
+    (flavor) => componentFlavorMeta[flavor].slinkityConfig.renderer,
+  )
+  const imports = selectedComponentFlavors.map(
+    (flavor) => componentFlavorMeta[flavor].slinkityConfig.importStatement,
+  )
+  return `import { defineConfig } from 'slinkity'
+${imports.join('\n')}
+
+export default defineConfig({
+  renderers: [${renderers.join(', ')}],
+})
+`
+}
 
 ;(async () => {
   let dest = process.argv[2]
@@ -18,11 +101,11 @@ const PKG = 'package.json'
     {
       type: 'multiselect',
       name: 'components',
-      message: 'Which flavor of components do you want?',
+      message: 'What flavor of components do you want, if any?',
       choices: [
-        { title: 'React', value: 'React' },
-        { title: 'Vue', value: 'Vue' },
-        { title: 'Svelte', value: 'Svelte' },
+        { title: 'React', value: 'react' },
+        { title: 'Vue', value: 'vue' },
+        { title: 'Svelte', value: 'svelte' },
       ],
     },
   ])
@@ -33,22 +116,75 @@ const PKG = 'package.json'
   // they must have ctrl + C'd out of the program
   if (!dest) process.exit(0)
 
-  const srcRoot = path.join(__dirname, 'template')
-  const destRoot = path.join(process.cwd(), dest)
-  fs.mkdirSync(destRoot)
+  const srcResolved = path.join(__dirname, 'template')
+  const destResolved = path.join(process.cwd(), dest)
+  fs.mkdirSync(destResolved)
 
-  // Set package name based on project name
-  const pkg = require(path.join(srcRoot, PKG))
+  // copy files to output
+  const templates = fs.readdirSync(srcResolved)
+  const componentTemplates = new Set(
+    Object.values(componentFlavorMeta).flatMap(({ exclusiveTemplates }) => exclusiveTemplates),
+  )
+  const selectedComponentTemplates = new Set(
+    promptResponses.components.flatMap(
+      (component) => componentFlavorMeta[component].exclusiveTemplates,
+    ),
+  )
+  for (const template of templates) {
+    const src = path.join(srcResolved, template)
+    const dest = path.join(destResolved, template)
+    copy(src, dest, (filePath) => {
+      // check whether or not we should copy the file
+      const relativePath = path.relative(srcResolved, filePath)
+      const isFileToPreprocess = filesToPreprocess.has(relativePath)
+      const isOmittedComponentTemplate =
+        componentTemplates.has(relativePath) && !selectedComponentTemplates.has(relativePath)
+      return isFileToPreprocess || isOmittedComponentTemplate
+    })
+  }
+
+  // package.json
+  const pkg = require(path.join(srcResolved, PKG))
   pkg.name = toValidPackageName(dest)
-  fs.writeFileSync(path.join(destRoot, PKG), JSON.stringify(pkg, null, 2))
+  for (const componentFlavor of promptResponses.components) {
+    pkg.dependencies = {
+      ...pkg.dependencies,
+      ...componentFlavorMeta[componentFlavor].packageJson.dependencies,
+    }
+    pkg.devDependencies = {
+      ...pkg.devDependencies,
+      ...componentFlavorMeta[componentFlavor].packageJson.devDependencies,
+    }
+  }
+  fs.writeFileSync(path.join(destResolved, PKG), JSON.stringify(pkg, null, 2))
 
-  // Copy all template files to dest
-  // TODO: add "plain" starter alongside "react"
-  const templateFilePaths = fs.readdirSync(srcRoot).filter((filePath) => filePath !== PKG)
-  for (const templateFilePath of templateFilePaths) {
-    const src = path.join(srcRoot, templateFilePath)
-    const dest = path.join(destRoot, templateFilePath)
-    copy(src, dest)
+  // index.md
+  const indexMd = fs.readFileSync(path.join(srcResolved, INDEX_MD)).toString()
+  const shortcodes = promptResponses.components.map(
+    (componentFlavor) => componentFlavorMeta[componentFlavor].shortcode,
+  )
+  fs.writeFileSync(
+    path.join(destResolved, INDEX_MD),
+    indexMd.replace('<!--insert-component-shortcodes-here-->', shortcodes.join('\n')),
+  )
+
+  // layout.njk
+  const layoutNjk = fs.readFileSync(path.join(srcResolved, LAYOUT_NJK)).toString()
+  const navLinks = promptResponses.components.map((componentFlavor) => {
+    const { href, text } = componentFlavorMeta[componentFlavor].navLink
+    return `<a href="${href}">${text}</a>`
+  })
+  fs.writeFileSync(
+    path.join(destResolved, LAYOUT_NJK),
+    layoutNjk.replace('<!--insert-nav-links-here-->', navLinks.join('\n      ')),
+  )
+
+  // slinkity.config.js
+  if (promptResponses.components.length) {
+    fs.writeFileSync(
+      path.join(destResolved, 'slinkity.config.js'),
+      toSlinkityConfigContents(promptResponses.components),
+    )
   }
 
   console.log(`Welcome to your first ${yellow('Slinkity site!')}`)
@@ -66,12 +202,13 @@ npm start
  * Inspired by https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
  * @param {string} src
  * @param {string} dest
+ * @param {(filePath: string) => boolean} shouldOmit
  */
-function copy(src, dest) {
+function copy(src, dest, shouldOmit) {
   const stat = fs.statSync(src)
   if (stat.isDirectory()) {
-    copyDir(src, dest)
-  } else {
+    copyDir(src, dest, shouldOmit)
+  } else if (!shouldOmit(src)) {
     fs.copyFileSync(src, dest)
   }
 }
@@ -81,13 +218,14 @@ function copy(src, dest) {
  * Inspired by https://github.com/vitejs/vite/blob/main/packages/create-vite/index.js
  * @param {string} srcDir
  * @param {string} destDir
+ * @param {(filePath: string) => boolean} shouldOmit
  */
-function copyDir(srcDir, destDir) {
+function copyDir(srcDir, destDir, shouldOmit) {
   fs.mkdirSync(destDir, { recursive: true })
   for (const file of fs.readdirSync(srcDir)) {
     const srcFile = path.resolve(srcDir, file)
     const destFile = path.resolve(destDir, file)
-    copy(srcFile, destFile)
+    copy(srcFile, destFile, shouldOmit)
   }
 }
 
