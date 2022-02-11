f6d83eb7d565cdd1f1c84dcf60756fbb1d31ba0d
Holben888 <bholmesdev@gmail.com>
2022-02-10 23:50:55 -0500

    fix: remove duplicate links from sections

--- www/src/docs/docs.11tydata.js
+++ www/src/docs/docs.11tydata.js
@@ -4,20 +4,33 @@ const { join } = require('path')
 const navSlugSortOrderGuides = [
   '',
   'quick-start',
-  'config',
   'component-shortcodes',
   'component-pages-layouts',
   'partial-hydration',
-  'styling',
-  'asset-management',
-  'import-aliases',
   'deployment',
 ]
 
-const navSlugSortOrderReference = ['partial-hydration', 'styling', 'import-aliases', 'deployment']
+const navSlugSortOrderReference = ['config', 'styling', 'asset-management', 'import-aliases']
 
 const trimSlashes = (s) => s.replace(/^\/|\/$/g, '')
 const toUrl = (slug) => trimSlashes(join('docs/', slug))
+/**
+ * Map a given page slug to a link based on a given collection
+ * @param {any[]} docs All docs from the "docs" 11ty collection
+ * @param {string} slug Page slug to match with titles and links
+ * @returns {{
+ *  href: string;
+ *  title: string;
+ * }}
+ */
+const mapToLinks = (docs, slug) => {
+  const matchingDocInfo = docs.find((doc) => trimSlashes(doc.data.page.url) === toUrl(slug))
+
+  return {
+    href: matchingDocInfo && matchingDocInfo.data.page.url,
+    title: matchingDocInfo && matchingDocInfo.data.title,
+  }
+}
 
 module.exports = {
   layout: 'docs',
@@ -25,22 +38,8 @@ module.exports = {
   tags: 'docs',
   eleventyComputed: {
     navLinksSorted: ({ collections: { docs } }) => {
-      const guides = navSlugSortOrderGuides.map((slug) => {
-        const matchingDocInfo = docs.find((doc) => trimSlashes(doc.data.page.url) === toUrl(slug))
-
-        return {
-          href: matchingDocInfo && matchingDocInfo.data.page.url,
-          title: matchingDocInfo && matchingDocInfo.data.title,
-        }
-      })
-      const reference = navSlugSortOrderReference.map((slug) => {
-        const matchingDocInfo = docs.find((doc) => trimSlashes(doc.data.page.url) === toUrl(slug))
-
-        return {
-          href: matchingDocInfo && matchingDocInfo.data.page.url,
-          title: matchingDocInfo && matchingDocInfo.data.title,
-        }
-      })
+      const guides = navSlugSortOrderGuides.map((slug) => mapToLinks(docs, slug))
+      const reference = navSlugSortOrderReference.map((slug) => mapToLinks(docs, slug))
 
       return {
         guides: {
