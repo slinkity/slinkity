72dd8ef9ce449b461b21087a41cd53a16f74f3c0
Benjamin Holmes <bholmesdev@gmail.com>
2022-02-16 22:22:05 -0500

    Fix: instructional logs to upgrade "render" -> "hydrate" (#180)
    * deps: add chalk to renderer-react
    
    * docs: explain getProps is no longer supported
    
    * docs: offer alternatives to "render"
    
    * docs: add note that getProps is not required
    
    * fix: remove stray console log
    
    * docs: note comps aren't hydrated by default
    
    * docs: simplify wording

--- packages/slinkity-renderer-react/index.js
+++ packages/slinkity-renderer-react/index.js
@@ -1,5 +1,6 @@
 const pkg = require('./package.json')
 const reactPlugin = require('@vitejs/plugin-react')
+const chalk = require('chalk')
 
 const client = `${pkg.name}/client`
 const server = `${pkg.name}/server`
@@ -42,6 +43,13 @@ module.exports = {
       useFormatted11tyData: true,
       async getData(inputPath) {
         const Component = await toCommonJSModule(inputPath)
+        if (Component.getProps !== undefined) {
+          console.log(
+            chalk.yellow(
+              `[Warning] The "getProps" function is no longer supported as of v0.6! If you intended to use "getProps" to generate props for "${inputPath}," try using "hydrate.props(...)" instead. You can also avoid hydrating your page to omit "getProps" entirely. See our docs for more: https://slinkity.dev/docs/component-pages-layouts`,
+            ),
+          )
+        }
         return Component.frontMatter
       },
     }
