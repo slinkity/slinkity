505f97821cd0ed673cf29a125f61f25a26c626c5
Holben888 <bholmesdev@gmail.com>
2022-02-16 21:47:29 -0500

    docs: explain getProps is no longer supported

--- packages/slinkity-renderer-react/index.js
+++ packages/slinkity-renderer-react/index.js
@@ -1,5 +1,6 @@
 const pkg = require('./package.json')
 const reactPlugin = require('@vitejs/plugin-react')
+const chalk = require('chalk')
 
 const client = `${pkg.name}/client`
 const server = `${pkg.name}/server`
@@ -42,6 +43,14 @@ module.exports = {
       useFormatted11tyData: true,
       async getData(inputPath) {
         const Component = await toCommonJSModule(inputPath)
+        console.log(Object.keys(Component))
+        if (Component.getProps !== undefined) {
+          console.log(
+            chalk.yellow(
+              `[Warning] The "getProps" function is no longer supported as of v0.6! If you intended to use "getProps" to generate props for your component page "${inputPath}," try using "hydrate.props(...)" instead. See our docs for more: https://slinkity.dev/docs/component-pages-layouts`,
+            ),
+          )
+        }
         return Component.frontMatter
       },
     }
