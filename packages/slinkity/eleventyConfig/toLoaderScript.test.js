21db4cd013d87814a7bbb457d950ab4fd2048865
Holben888 <bholmesdev@gmail.com>
2022-03-26 14:10:17 -0400

    chore: add toLoaderScript test

---
+++ packages/slinkity/eleventyConfig/toLoaderScript.test.js
@@ -0,0 +1,34 @@
+const loaders = require('./loaders')
+const toLoaderScript = require('./toLoaderScript')
+
+const loaderMap = Object.fromEntries(loaders.map((loader) => [loader.name, loader]))
+
+function toFakeArgs(loader) {
+  if (loader === 'onClientMedia') {
+    return 'screen (max-width: 500px)'
+  } else {
+    return ''
+  }
+}
+
+describe('toLoaderScript', () => {
+  let id = 0
+
+  afterEach(() => {
+    id += 1
+  })
+  it.each(Object.keys(loaderMap))('Should generate hydration script for "%s"', (loader) => {
+    const fakeArgs = toFakeArgs(loader)
+    expect(
+      toLoaderScript({
+        componentPath: `/component/using/${loader}/index.jsx`,
+        componentLoaderMap: loaderMap,
+        loader: fakeArgs ? `${loader}(${fakeArgs})` : loader,
+        id,
+        props: { hydrate: loader },
+        clientRenderer: '@slinkity/renderer-react',
+        children: `<p>I love the ${loader} loader!</p>`,
+      }),
+    ).toMatchSnapshot()
+  })
+})
