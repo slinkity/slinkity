68bd452eae04df4c2c6dc89ac8ff93e4f4b32559
Holben888 <bholmesdev@gmail.com>
2022-03-26 14:59:55 -0400

    fix: update renderers to match new "loader" flag

--- packages/slinkity-renderer-react/client.js
+++ packages/slinkity-renderer-react/client.js
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import StaticHtml from './StaticHtml'
 
-export default function renderComponent({ Component, target, props, children }) {
-  ReactDOM.render(
-    React.createElement(Component, props, React.createElement(StaticHtml, { value: children })),
-    target,
+export default function renderComponent({ Component, target, props, children, isSSR }) {
+  const element = React.createElement(
+    Component,
+    props,
+    React.createElement(StaticHtml, { value: children }),
   )
+  if (isSSR) {
+    ReactDOM.hydrate(element, target)
+  } else {
+    ReactDOM.render(element, target)
+  }
 }
--- packages/slinkity-renderer-svelte/client.js
+++ packages/slinkity-renderer-svelte/client.js
@@ -1,9 +1,9 @@
 import SvelteWrapper from './Wrapper.svelte'
 
-export default function client({ Component, target, props, children }) {
+export default function client({ Component, target, props, children, isSSR }) {
   new SvelteWrapper({
     target,
     props: { __slinkity_component: Component, __slinkity_children: children, ...props },
-    hydrate: true,
+    hydrate: isSSR,
   })
 }
