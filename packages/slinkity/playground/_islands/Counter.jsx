d2e23f28ef5f4f0128d6bf48b10849e6f513c9f5
bholmesdev <bholmesdev@gmail.com>
2022-08-20 18:03:40 -0400

    play: try is-land

---
+++ packages/slinkity/playground/_islands/Counter.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'preact/hooks'
+
+export default function Counter({ initialCount }) {
+  const [count, setCount] = useState(initialCount ?? 0)
+  return (
+    <>
+      <p>Oh, and the state is preserved while I work {count}</p>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </>
+  )
+}
