55b55230311d7b36d0d77267eb4485766d20b64e
bholmesdev <bholmesdev@gmail.com>
2022-08-20 16:40:41 -0400

    refactor: use function for renderers

--- packages/slinkity-preact/index.cjs
+++ packages/slinkity-preact/index.cjs
@@ -4,14 +4,14 @@ const { default: vitePreact } = require('@preact/preset-vite')
 const client = `${pkg.name}/client`
 const server = `${pkg.name}/server`
 
-module.exports = {
-  name: 'preact',
-  extensions: ['jsx', 'tsx'],
-  client,
-  server,
-  viteConfig() {
-    return {
+module.exports = function slinkityPreact() {
+  return {
+    name: 'preact',
+    extensions: ['jsx', 'tsx'],
+    client,
+    server,
+    viteConfig: {
       plugins: [vitePreact()],
-    }
-  },
+    },
+  }
 }
