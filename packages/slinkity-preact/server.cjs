79af3ab003fe14417cc8f5e81d8dbb00c23a7a38
bholmesdev <bholmesdev@gmail.com>
2022-08-20 16:41:08 -0400

    fix: module.exports for preact server

--- packages/slinkity-preact/server.cjs
+++ packages/slinkity-preact/server.cjs
@@ -1,7 +1,7 @@
 const { h } = require('preact')
 const renderToString = require('preact-render-to-string')
 
-export default async function server({ Component, props }) {
+module.exports = function server({ Component, props }) {
   const vnode = h(Component.default, props)
   return {
     html: renderToString(vnode),
