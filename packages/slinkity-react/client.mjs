a102841cc8e93ba990d0a60a9f3777e7d3b99fdb
bholmesdev <bholmesdev@gmail.com>
2023-01-08 17:01:15 -0500

    feat: new react renderer

---
+++ packages/slinkity-react/client.mjs
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function client({ Component, target, props, isClientOnly }) {
+  const element = React.createElement(
+    Component,
+    props,
+  )
+  if (isClientOnly) {
+    ReactDOM.render(element, target)
+  } else {
+    ReactDOM.hydrate(element, target)
+  }
+}
---
+++ packages/slinkity-react/index.cjs
@@ -0,0 +1,34 @@
+const pkg = require('./package.json')
+const ssr = require('./ssr.cjs')
+const { default: viteReact } = require('@vitejs/plugin-react')
+
+module.exports = function slinkityReact() {
+  return {
+    name: 'react',
+    extensions: ['jsx', 'tsx'],
+    clientEntrypoint: `${pkg.name}/client`,
+    ssr,
+    viteConfig: {
+      plugins: [viteReact()],
+      optimizeDeps: {
+        include: ['react', 'react/jsx-runtime', 'react/jsx-dev-runtime', 'react-dom'],
+      },
+      resolve: {
+        dedupe: ['react', 'react-dom'],
+      },
+      ssr: {
+        external: ['react-dom/server.js'],
+      },
+    },
+    page({ Component }) {
+      return {
+        getData() {
+          return Component.frontmatter ?? Component.frontMatter ?? {}
+        },
+        getIslandMeta() {
+          return Component.island
+        },
+      }
+    },
+  }
+}
